4d09040f2d8fec7031bfc6f2a675d25effec9251
riduanul <riduanul.haque1@gmail.com>
2024-01-14 17:03:05 +0600

    push

--- health_care/settings.py
+++ health_care/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECURITY_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['https://health-care-buts.onrender.com/', 'https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://health-care-buts.onrender.com/', 'https://*.127.0.0.1', 'https://health-care-buts.onrender.com', 'https://health-care-buts.onrender']
 
 # Application definition
 
